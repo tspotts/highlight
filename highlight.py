58e2c9ea48d6138633c1694116a5ed9f0d2550c6
Terry Spotts <tspotts@etsy.com>
2023-08-09 16:07:35 -0400

    DEV environment setup and 1st Prototypee

---
+++ highlight.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+from rich.console import Console
+from rich.highlighter import RegexHighlighter
+from rich.theme import Theme
+
+
+class Highlighter(RegexHighlighter):
+    highlights = [r"(?i)(?P<and>\band\b)", r"(?i)(?P<of>\bof\b)", r"(?i)(?P<the>\bthe\b)"]
+
+
+theme = Theme({"and": "blue", "of": "red", "the": "green"})
+console = Console(highlight=False, theme=theme)
+
+highlight_text = Highlighter()
+
+for line in fileinput.input(encoding="utf-8"):
+    console.print(highlight_text(line), end=None)
