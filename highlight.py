1c4a38de1c8790b3c2af9138b638f6e8349187c8
Terry Spotts <tspotts@etsy.com>
2023-08-10 09:53:11 -0400

    nix poetry and rich for traditional pyproject.toml and only use click(?)

--- highlight.py
+++ highlight.py
@@ -1,23 +1,11 @@
-#!/usr/bin/env python3
-
 import fileinput
 
 import click
-from rich.console import Console
-from rich.highlighter import RegexHighlighter
-from rich.theme import Theme
-
-
-class Highlighter(RegexHighlighter):
-    highlights = [r"(?i)(?P<and>\band\b)", r"(?i)(?P<of>\bof\b)", r"(?i)(?P<the>\bthe\b)"]
-
-
-theme = Theme({"and": "blue", "of": "red", "the": "green"})
-console = Console(highlight=False, theme=theme)
-highlight_text = Highlighter()
 
 
 @click.command()
+@click.version_option()
 def cli():
     for line in fileinput.input(encoding="utf-8"):
-        console.print(highlight_text(line), end=None)
+        click.echo(line, nl=False)
+        # click.echo(click.style(line, fg="green") if "and" in line else line, nl=False)
